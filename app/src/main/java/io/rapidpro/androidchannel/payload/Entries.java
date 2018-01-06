8b25d823a0e4ec36b60f491aa47863b95507eef5
James Nielsen <coderinlove@gmail.com>
2018-01-06 00:00:33 -0300

    Add dropbox data layer

---
+++ app/src/main/java/io/rapidpro/androidchannel/payload/Entries.java
@@ -0,0 +1,9 @@
+package io.rapidpro.androidchannel.payload;
+
+
+import java.util.List;
+
+public class Entries<T> {
+
+    public List<T> entries;
+}
---
+++ app/src/main/java/io/rapidpro/androidchannel/payload/File.java
@@ -0,0 +1,12 @@
+package io.rapidpro.androidchannel.payload;
+
+public class File {
+
+    public String id;
+    public String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
