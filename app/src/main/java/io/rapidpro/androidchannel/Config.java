a4797400a066618a7199175ac35dad3cc292f9f3
Teeh Amaral <contato@teehamaral.me>
2020-10-28 19:00:55 -0300

    Changes for dev.ccl

--- app/src/main/java/io/rapidpro/androidchannel/Config.java
+++ app/src/main/java/io/rapidpro/androidchannel/Config.java
@@ -2,11 +2,10 @@ package io.rapidpro.androidchannel;
 
 public class Config {
     //This is the URL of your RapidPRO instance
-//    public static final String ENDPOINT = "https://hs.communityconnectlabs.com";
-    public static final String ENDPOINT = "https://hs.communityconnectlabs.com";
+    public static final String ENDPOINT = "https://dev.communityconnectlabs.com";
 
 
 
     //RapidPRO name, in the case that you have a different branding
-    public static final String RAPIDPRO_NAME = "Community Connect Labs";
+    public static final String RAPIDPRO_NAME = "CommunityConnect Labs";
 }
